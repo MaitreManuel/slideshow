22477db7065cec19525956212db66c6f8b2031a3
Trystan Eveno <trystan.eveno@lephare.com>
2018-02-23 15:09:00 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,197 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const nodeModules = path.resolve(__dirname, 'node_modules');
+
+const resources = path.resolve(__dirname, 'src/SiteBundle/Resources');
+
+const config = {
+  cache: true,
+  entry: {
+    app: [
+      './src/Resources/assets/app.js',
+    ]
+  },
+  output: {
+    library: '[name]', // assets build
+    libraryTarget: 'umd',
+    path: path.join(__dirname, 'web/compiled'),
+    filename: '[name].js',
+    chunkFilename: '[name]-[chunkhash].js'
+  },
+  module: {
+    rules: [{
+      // required for es6
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react']
+        }
+      }
+    }, {
+      // required to write 'require('./style.scss')'
+      test: /\.s(a|c)ss$/,
+      loader: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              sourceMap: true
+            }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: []
+            }
+          }, {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [
+                path.resolve(resources , '/assets/'),
+                nodeModules
+              ],
+              sourceMap: true
+            }
+          },
+        ]
+      })
+    }, {
+      // required to write 'require('./style.css')'
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      })
+    }, {
+      // required for bootstrap icons
+      test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url-loader',
+      options: {
+        prefix: 'font/',
+        limit: 5000,
+        mimetype: 'application/font-woff'
+      }
+    }, {
+      test: /\.(ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file-loader',
+      options: {
+        prefix: 'font/'
+      }
+    }, {
+      // required to support images
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            hash: 'sha512',
+            digest: 'hex',
+            name: '[hash].[ext]'
+          }
+        },
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            includePaths: [
+              path.resolve(resources , '/public/')
+            ],
+            gifsicle: {
+              interlaced: false
+            },
+            bypassOnDebug: true,
+            optipng: {
+              optimizationLevel: 7
+            }
+          }
+        }
+      ]
+    }, {
+      // Expose jQuery
+      test: require.resolve('jquery'),
+      use: [
+        {
+          loader: 'expose-loader?jQuery'
+        }, {
+          loader: 'expose-loader?$'
+        }
+      ]
+    }]
+  },
+  // devtool: 'cheap-module-eval-source-map',
+  devtool: 'source-map',
+  // resolve: {
+  //   alias: {
+  // https://github.com/vuejs-templates/webpack/issues/215
+  // vue: 'vue/dist/vue.common.js'
+  // }
+  // },
+  plugins: [
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'vendors',
+    //   filename: 'vendors.js'
+    // }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // Use the plugin to specify the resulting filename (and add needed behavior to the compiler)
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      allChunks: true
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'web'),
+    compress: true,
+    port: 9000,
+    host: '0.0.0.0'
+  },
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.devtool= false;
+  config.plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        // eslint-disable-next-line camelcase
+        screw_ie8: true,
+        warnings: false
+      }
+    }),
+    // optimize module ids by occurrence count
+    new webpack.optimize.OccurrenceOrderPlugin()
+  );
+} else {
+  config.plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+    })
+  );
+}
+
+module.exports = config;
