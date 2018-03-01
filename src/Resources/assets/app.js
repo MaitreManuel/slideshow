67e4ae1204061531a7da4a374ab2f493c0a15f26
Trystan Eveno <trystan.eveno@lephare.com>
2018-03-01 19:01:28 +0100

    fix call to class SlideManager

--- src/Resources/assets/app.js
+++ src/Resources/assets/app.js
@@ -4,15 +4,15 @@ import Admin from './admin/admin';
 import slideManager from './admin/slideManager';
 
 // import styles
-import './main.scss';  
+import './main.scss';
 
 // DOM Ready
 window.onload = () => {
   Slideshow.init();
   Admin.init();
 
-  new slideManager();
-  const slides = slideManager.getSlides();
+  let sManager = new slideManager();
+  const slides = sManager.getSlides();
   slides.then((e) => {
     console.log(e.val());
   });
