ef5eab486e03e40b2cadf628c10cb0dccb27f986
Herve <h.hochart29@gmail.com>
2018-03-01 11:44:48 +0100

    admin start

---
+++ src/Resources/assets/utils.js
@@ -0,0 +1,11 @@
+export default class Utils {
+
+  static toggle (el, className) {
+    if (el.classList.contains(className)) {
+      el.classList.remove(className);
+    } else {
+      el.classList.add(className);
+    }
+  }
+
+}
