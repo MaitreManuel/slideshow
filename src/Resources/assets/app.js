62ed1f363860aa43f5b8408535ba279b9306b42c
Herve <h.hochart29@gmail.com>
2018-03-06 12:17:27 +0100

    removed some code

--- src/Resources/assets/app.js
+++ src/Resources/assets/app.js
@@ -1,20 +1,22 @@
 // import scripts
+import * as Slideshow from './slideshow/initialize';
 import Admin from './admin/admin';
+import slideManager from './admin/slideManager';
 
 // import styles
 import './main.scss';
 
 // DOM Ready
 window.onload = () => {
+  Slideshow.init();
   Admin.init();
 
   let sManager = new slideManager();
-  const getSlides = sManager.getSlides();
-  getSlides.then((e) => {
-    console.log(e.val());
-  });
 
-  sManager.login('herve@slideshow.fr', 'slideshow')
-    .then(sManager.addSlide('Titre de la slide ajoutee', 'Description de la slide', 'url href', 'src'))
-    .then(sManager.logout());
+  /*
+  * pour tester le login / set / logout
+  * */
+  //sManager.login('herve@slideshow.fr', 'slideshow')
+  //  .then(sManager.addSlide('Titre de la slide ajoutee', 'Description de la slide', 'url href', 'src'))
+  //  .then(sManager.logout());
 };
