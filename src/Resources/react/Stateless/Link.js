87d14e02643500a8ed08ec43ac56e977830356ec
Trystan Eveno <trystan.eveno@lephare.com>
2018-03-08 16:13:07 +0100

    quicksave

--- src/Resources/react/Stateless/Link.js
+++ src/Resources/react/Stateless/Link.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 
 const slides_link = document.querySelector('#slides-link');
 
-const Image = ({ link }) => {
+const Image = ({ link, title }) => {
   let template =
-    <div className="container">
-      <div className="label">
-        <span className="tooltip-toggle" aria-label="Sample text for your tooltip!" tabIndex="0">
-          <a href={ link } target="_blank"></a>
-        </span>
-      </div>
+    <div className="item">
+      <a href={ link } target="_blank" className="links">{ 'Page vers '+ title }</a>
     </div>
   ;
 
